9e0eb170123142c1986425b8ab96da88e529cc42
SuLiang <82970607@qq.com>
2022-04-19 19:58:54 +0800

    修复redis配置注入bug

--- src/main/java/com/example/backend_template/config/RedisConfigurer.java
+++ src/main/java/com/example/backend_template/config/RedisConfigurer.java
@@ -2,15 +2,17 @@ package com.example.backend_template.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @ClassName RedisConfigurer Redis的配置类
@@ -22,21 +24,23 @@ import redis.clients.jedis.JedisPoolConfig;
 @EnableCaching      //开启缓存
 public class RedisConfigurer extends CachingConfigurerSupport {
 
+    /**
+     * redis 的各种配置信息，由框架注入
+     */
+    @Autowired
+    private RedisProperties redisProperties;
+
+    /**
+     * 日志工具
+     */
     private static final Logger log = LoggerFactory.getLogger(RedisConfigurer.class);
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.redis")
-    public JedisPoolConfig getRedisConfig() {
-        JedisPoolConfig config = new JedisPoolConfig();
-        return config;
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.redis")
     public JedisConnectionFactory getConnectionFactory() {
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        JedisPoolConfig config = getRedisConfig();
-        factory.setPoolConfig(config);
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration);
         log.info("The hostname of the redis connection is：{}, and the port is: {}", factory.getHostName(), factory.getPort());
         return factory;
     }
